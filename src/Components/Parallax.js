7c27861835e8f12e7b83ffa46ae465a7cf838050
Alexvz2 <alexvzvz@yahoo.com>
2019-06-19 23:09:23 -0700

    Added TypeWriter Effect

--- src/Components/Parallax.js
+++ src/Components/Parallax.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {ParallaxBanner, ParallaxProvider} from 'react-scroll-parallax';
 import styles from './Parallax.css';
+import Typist from 'react-typist';
 //import './App.css'
 
 
@@ -12,7 +13,7 @@ const parallaxLayers =[
     },
     {
         image: require('./../img/ParallaxImg/4BackM.png'),
-        amount:0.05,
+        amount:0.0,
         expanded: false,
     },
     {
@@ -22,13 +23,19 @@ const parallaxLayers =[
     },
     {
         image: require('./../img/ParallaxImg/2BackTrees.png'),
-        amount:0.07,
+        amount:0.08,
         expanded: false,
     },
     {
         image: require('./../img/ParallaxImg/1FrontN.png'),
         amount:0.12,
         expanded: false,
+        
+    },
+    {
+        amount:0,
+        expanded: false,
+        children: (<Typist style={{fontSize: '10vh', color:'#000000', textAlign: 'center', lineHeight: '120vh' }}>Hello, My Name is Alex</Typist>),
     },
     
 ];
@@ -42,11 +49,10 @@ class ParallaxHeader extends Component {
                     <ParallaxBanner
                         className={styles.bannerBg}
                         layers={parallaxLayers}
-                        style={{ height: '100vh' }}
-                    >
-                        <div className={styles.parallaxChildren}>
-                            <h1>Hello, My Name is Alex</h1>
-                        </div>
+                        style={{ height: '100vh' }}>
+                        
+                        <h1 style={{fontSize: '10vh', color:'#000000', textAlign: 'center', lineHeight: '120vh' }}>Text to Show</h1>
+                        <Typist style={{fontSize: '10vh', color:'#000000', textAlign: 'center', lineHeight: '120vh' }}>Animated Text to Show</Typist>
                     </ParallaxBanner>
                 </div>
             </div>
