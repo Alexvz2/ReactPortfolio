993e0a803aba08c3cd65bae647139fe2bfdab56c
Alexvz2 <alexvzvz@yahoo.com>
2019-06-23 22:14:54 -0700

    Modified about page

--- src/Components/About.js
+++ src/Components/About.js
@@ -18,7 +18,7 @@ class About extends Component {
                     <div >
                         <h2>About Me.</h2>
                         <p className="lead">My name is Alex Vazquez, I'm a UBC Engineering ECE student
-                        based in Vancouver, BC. I graduated from high school with a Full IB Diploma
+                        based in Vancouver, BC. I graduated from high school with a Science based IB Diploma
                         from West Vancouver Secondary School in Vancouver.</p>
                         <p className="lead"> I am passionate about the intersection between Electrical and Computer Engineering, currently, I'am a Software Engineer(Explore) working for Microsoft in Redmond, while working on the Comms system of the BC Satellite, in Charge of decoding the telemetry data.</p>
                         <p className="lead">During school, I am a Senior Controls Engineer in UBC Orbit, Attitude Determination and Control Systems Sub-team,
